d66b0449ba0823ee73813d7897e0a9b1c7b473ae
yanchokraev <54913325+yanchokraev@users.noreply.github.com>
2021-07-20 14:58:11 +0300

    API Version 1.0.0

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API...' })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
